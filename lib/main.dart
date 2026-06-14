5fb9bfc9bcfa204a8203120dc9b4c80ecaee8019
hiofadlikaakbar <hioakbar1@gmail.com>
2026-06-14 11:07:41 +0700

    rombak main dart ke settingan baru

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'features/auth/login_page.dart';
 import 'package:provider/provider.dart';
-import './features/providers/cart_provider.dart';
+
+import 'firebase_options.dart';
+
+import 'features/auth/login_page.dart';
 import 'features/auth/signup_page.dart';
-import 'features/splash/splash_screen.dart';
-import 'features/home/home_page.dart';
 import 'features/cart/cart_page.dart';
+import 'features/home/home_page.dart';
+import 'features/providers/cart_provider.dart';
+import 'features/splash/splash_screen.dart';
 
 Future<void> firebaseBackgroundHandler(RemoteMessage message) async {
-  print("Background notif: ${message.notification?.title}");
+  debugPrint("Background notif: ${message.notification?.title}");
 }
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyCQa8o5lQc8Izf1lPwxwcAkbI9Yt7Qwwro",
-      authDomain: "marketplace-uts.firebaseapp.com",
-      projectId: "marketplace-uts",
-      storageBucket: "marketplace-uts.firebasestorage.app",
-      messagingSenderId: "608464027335",
-      appId: "1:608464027335:web:6b8f04fbb00fdbaeb9c8e2",
-    ),
-  );
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   FirebaseMessaging.onBackgroundMessage(firebaseBackgroundHandler);
+
   await FirebaseMessaging.instance.requestPermission();
-  runApp(
-    MultiProvider(
+
+  runApp(const AppProvider());
+}
+
+class AppProvider extends StatelessWidget {
+  const AppProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => CartProvider())],
       child: const MyApp(),
-    ),
-  );
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -42,9 +47,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
+      title: 'Hio Marketplace UAS',
       initialRoute: '/splash',
-
       routes: {
         '/splash': (context) => const SplashScreen(),
         '/login': (context) => const LoginPage(),
