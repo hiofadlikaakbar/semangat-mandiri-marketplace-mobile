8e7e8c0e5ee1e1d57b73aff7d650cd41f48697a2
hiofadlikaakbar <hioakbar1@gmail.com>
2026-06-14 12:56:41 +0700

    import payment dicheckout

--- lib/features/cart/cart_page.dart
+++ lib/features/cart/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart_provider.dart';
+import '../payment/checkout_page.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
